852cea49e967c08bd9b9f59e431d536e972b2291
Clara Tsang <clarat@sfu.ca>
2016-03-16 13:40:46 -0700

    Added main menu
    modified main menu activity
    - got rid of toolbar/default unnecessary stuff
    - added buttons to navigate to other activities

--- app/src/main/java/com/pinnaclebiometrics/floecompanionapp/FloeMainMenuAct.java
+++ app/src/main/java/com/pinnaclebiometrics/floecompanionapp/FloeMainMenuAct.java
@@ -1,13 +1,10 @@
 package com.pinnaclebiometrics.floecompanionapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
 public class FloeMainMenuAct extends AppCompatActivity {
 
@@ -15,38 +12,40 @@ public class FloeMainMenuAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floe_main_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button calBtn = (Button) findViewById(R.id.calBtn);
+        Button rtBtn = (Button) findViewById(R.id.rtBtn);
+        Button recBtn = (Button) findViewById(R.id.recBtn);
+        Button revBtn = (Button) findViewById(R.id.revBtn);
+
+        calBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(FloeMainMenuAct.this,FloeCalibrationAct.class));
             }
         });
+
+        rtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FloeMainMenuAct.this, FloeRTFeedbackAct.class));
+            }
+        });
+
+        recBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FloeMainMenuAct.this, FloeRecordingAct.class));
+            }
+        });
+
+        revBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FloeMainMenuAct.this, FloeReviewListAct.class));
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_floe_main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
