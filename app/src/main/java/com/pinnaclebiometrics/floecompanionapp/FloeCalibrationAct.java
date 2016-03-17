5ec200eda9d723d3a41f3a9849409e61a9ea0b2e
Clara Tsang <clarat@sfu.ca>
2016-03-16 19:48:18 -0700

    bound stuff
    bound FloeDataTransmissionSvc to:
    -FloeRecordingAct
    -FloeRTFeedbackAct
    -FloeCalibrationAct
    
    bound FloeBLESvc to FloeDataTransmissionSvc

--- app/src/main/java/com/pinnaclebiometrics/floecompanionapp/FloeCalibrationAct.java
+++ app/src/main/java/com/pinnaclebiometrics/floecompanionapp/FloeCalibrationAct.java
@@ -1,30 +1,48 @@
 package com.pinnaclebiometrics.floecompanionapp;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.app.ProgressDialog;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.pinnaclebiometrics.floecompanionapp.FloeDataTransmissionSvc.FloeDTBinder;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FloeCalibrationAct extends AppCompatActivity {
+
+    FloeDataTransmissionSvc dataService;
+
+    public void getWeight(View view) {
+        //testing stuff; replace with actual thing
+        String test = dataService.Test();
+        TextView testText = (TextView) findViewById(R.id.testText);
+        testText.setText(test);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floe_calibration);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent i = new Intent(this, FloeDataTransmissionSvc.class);
+        bindService(i, dataConnection, Context.BIND_AUTO_CREATE);
     }
+
+    private ServiceConnection dataConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            FloeDTBinder binder = (FloeDTBinder) service;
+            dataService = binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
 }
