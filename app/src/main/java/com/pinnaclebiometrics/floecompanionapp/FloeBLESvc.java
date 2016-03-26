7cb9a5904a1d9d2de602991b473a699c4fb0fec4
LouisRoux <louis.roux@hotmail.com>
2016-03-25 23:24:35 -0700

    BLE Service first steps
    Started work on BLE service, including some modifications to the
    manifest, a new activity to display available BLE devices, and a bit of
    other stuff

--- app/src/main/java/com/pinnaclebiometrics/floecompanionapp/FloeBLESvc.java
+++ app/src/main/java/com/pinnaclebiometrics/floecompanionapp/FloeBLESvc.java
@@ -1,31 +1,57 @@
 package com.pinnaclebiometrics.floecompanionapp;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
-public class FloeBLESvc extends Service {
-
+public class FloeBLESvc extends Service
+{
     private final IBinder bleBinder = new FloeBLEBinder();
+    private boolean devicesTethered = false;
 
-    public FloeBLESvc() {
+    //TODO: figure out if we need multiple adapters/managers (one per foot, or not?)
+    final private BluetoothManager bleManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+    private BluetoothAdapter bleAdapter = bleManager.getAdapter();//TODO: Change minimum API version to 18
+
+    public FloeBLESvc()
+    {
+        //empty constructor
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent)
+    {
+        //check if bluetooth is enabled and prompt to enable if it isn't
+        if(bleAdapter == null || !bleAdapter.isEnabled())
+        {
+            Log.e("BLESvc", "Bluetooth is not enabled");
+            Intent enableBLEIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBLEIntent, REQUEST_ENABLE_BT);//TODO: put this check in the activity that needs to use BLE
+        }
+
         return bleBinder;
     }
 
-    //testing fxn
-    public String Test() {
-        String s = "it works!";
-        return s;
-    }
-
-    public class FloeBLEBinder extends Binder {
-        FloeBLESvc getService(){
+    public class FloeBLEBinder extends Binder
+    {
+        FloeBLESvc getService()
+        {
             return FloeBLESvc.this;
         }
+    }
+
+
+
+
+    //testing fctn
+    public String Test()
+    {
+        String s = "it works!";
+        return s;
     }
 }
--- app/src/main/java/com/pinnaclebiometrics/floecompanionapp/FloeDataTransmissionSvc.java
+++ app/src/main/java/com/pinnaclebiometrics/floecompanionapp/FloeDataTransmissionSvc.java
@@ -10,52 +10,75 @@ import android.os.Binder;
 import com.pinnaclebiometrics.floecompanionapp.FloeBLESvc.FloeBLEBinder;
 
 
-public class FloeDataTransmissionSvc extends Service {
+public class FloeDataTransmissionSvc extends Service
+{
 
     FloeBLESvc bleService;
+    private boolean bleSvcBound = false;
     private final IBinder dataTranBinder = new FloeDTBinder();
 
     //constructor
-    public FloeDataTransmissionSvc() {
+    public FloeDataTransmissionSvc()
+    {
+        //empty
     }
 
 
     //set up to be bound
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent)
+    {
         return dataTranBinder;
     }
 
     //happens once: this service is bound to BLE service
-    public void onCreate() {
+    @Override
+    public void onCreate()
+    {
         Intent i = new Intent(this, FloeBLESvc.class);
         bindService(i, bleConnection, Context.BIND_AUTO_CREATE);
     }
 
+
+    @Override
+    public void onDestroy()
+    {
+        //This makes sure to unbind the bleSvc to avoid leaking a ServiceConnection
+        //TODO: make sure every bound service gets unbound when its client stops
+        bleService.unbindService(bleConnection);
+    }
+
     //testing fxn
-    public String Test() {
+    public String Test()
+    {
         String s = "it works!";
         return s;
     }
 
     // create binder
-    public class FloeDTBinder extends Binder {
-        FloeDataTransmissionSvc getService(){
+    public class FloeDTBinder extends Binder
+    {
+        FloeDataTransmissionSvc getService()
+        {
             return FloeDataTransmissionSvc.this;
         }
     }
 
     //bind to BLE service
-    private ServiceConnection bleConnection = new ServiceConnection() {
+    private ServiceConnection bleConnection = new ServiceConnection()
+    {
         @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
             FloeBLEBinder binder = (FloeBLEBinder) service;
             bleService = binder.getService();
+            bleSvcBound = true;
         }
 
         @Override
-        public void onServiceDisconnected(ComponentName name) {
-
+        public void onServiceDisconnected(ComponentName name)
+        {
+            bleSvcBound = false;
         }
     };
 }
